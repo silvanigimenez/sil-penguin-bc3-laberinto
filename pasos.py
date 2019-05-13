44e6936c1ee37111d37bfbde990e673a3b7db7c1
silvanigimenez <silvanigimenez@gmail.com>
2019-05-13 12:48:37 -0400

    se agrego un comentario

---
+++ pasos.py
@@ -0,0 +1,15 @@
+#######################
+#comandos de GIT
+################33
+
+#git.add . 
+#git commit -m "el mensaje"
+#git push       (para empujar el codigo del local al Remoto)
+#git pull       (para estirar el codigo del Remoto al local)
+##################
+#comandos para navegar en la terminal
+##############
+
+#ls o dir (para ver los documentos existentes)
+#cd nombre_de_archivo (para entrar en el archivo)
+#cd  .. (para salir del archivo)
